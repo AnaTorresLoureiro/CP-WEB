81db29241e6be25c00ad8442ec8a47bcb1babe62
Lana <al.t.loureiro0@gmail.com>
2024-09-07 05:30:23 -0300

    rotas

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,12 +1,52 @@
-import { MainGeral } from "../../styled"
+import { MainGeral } from "../../styled";
 import Banner from '../../assets/banner.png';
+import Card from '../../components/Card.jsx';
 
-export default function Home(){
+// Importando as imagens corretamente
+import ArqueriaImage from '../../assets/arqueria.jpg';
+import EsgrimaImage from '../../assets/esgrima.jpg';
+import CanoagemImage from '../../assets/kayak.jpg';
+import ParaquedaImage from '../../assets/paraquedas.jpg';
 
-    return(
+export default function Home() {
+    const cardData = [
+        {
+            imageSrc: ArqueriaImage,
+            title: 'TERRESTRE',
+            description: 'Desafie os limites da sua força e velocidade com nosso equipamento de esportes terrestres.',
+        },
+        {
+            imageSrc: CanoagemImage,
+            title: 'MARINHO',
+            description: 'Conquiste o oceano com nosso material de alta performance para esportes aquáticos.',
+        },
+        {
+            imageSrc: ParaquedaImage,
+            title: 'AÉREO',
+            description: 'Sinta a liberdade nas alturas com nosso equipamento de esportes aéreos.',
+        },
+        {
+            imageSrc: EsgrimaImage,
+            title: 'COMBATE',
+            description: 'Prepare-se para o combate com nossa seleção de equipamentos.',
+        },
+    ];
+    return (
         <MainGeral>
             <img src={Banner} alt="" />
             <h1>ALCANCE O TOPO E SUPERE SEUS LIMITES</h1>
+            <p>MODALIDADE</p>
+            <hr/>
+            <div className="container">
+                {cardData.map((card, index) => (
+                    <Card
+                        key={index}
+                        imageSrc={card.imageSrc}
+                        title={card.title}
+                        description={card.description}
+                    />
+                ))}
+            </div>
         </MainGeral>
-    )
+    );
 }
--- src/routes/Produtos/index.jsx
+++ src/routes/Produtos/index.jsx
@@ -1,8 +1,54 @@
-export default function Produtos(){
+// Produtos.jsx
+import React from 'react';
+import styled from 'styled-components';
+import { MainProduto } from '../../styled'; 
+import Search from '../../assets/search.png';
+import Buy from '../../components/Buy.jsx';
+import Carousel from '../../components/Carrosel'; 
 
-    return(
-        <main>
-            <h1>Produtos</h1>
-        </main>
-    )
+import ArcoImage from '../../assets/arco.png';
+import PranchaImage from '../../assets/prancha.png';
+import VelaImage from '../../assets/vela.png';
+import LuvaImage from '../../assets/luva.png';
+
+export default function Produtos() {
+  const buyData = [
+    {
+      imageSrc: ArcoImage,
+      title: 'ARCO RECURVO',
+    },
+    {
+      imageSrc: PranchaImage,
+      title: 'PRANCHA DE SURFE',
+    },
+    {
+      imageSrc: VelaImage,
+      title: 'PARAPENTE',
+    },
+    {
+      imageSrc: LuvaImage,
+      title: 'LUVAS DE BOX',
+    },
+  ];
+
+  return (
+    <MainProduto>
+      <div className="pesquisa">
+        <input type="text" placeholder="Busque o que deseja" />
+        <button>
+          <img src={Search} alt="Search Icon" />
+        </button>
+      </div>
+
+      <h1>MARCAS</h1>
+      <Carousel />
+      <h2>PRODUTOS</h2>
+      <hr />
+      <div className="container">
+        {buyData.map((buy, index) => (
+          <Buy key={index} imageSrc={buy.imageSrc} title={buy.title} />
+        ))}
+      </div>
+    </MainProduto>
+  );
 }
